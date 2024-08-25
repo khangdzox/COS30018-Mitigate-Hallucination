8a146c238cc9f4c9e6840ceb2409c327ff523c76
Pink <honganhnguyen915@gmail.com>
2024-08-25 10:19:21 +1000

    Added sample_1.py file

---
+++ Finetuning/sample_1.py
@@ -0,0 +1,17 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
+import torch
+
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", torch_dtype=torch.bfloat16)
+streamer = TextStreamer(tokenizer) # type: ignore
+
+input_text = 'Ayyo whatsup bro ?'
+input_tokens = tokenizer(input_text, return_tensors="pt").to(model.device)
+
+output_tokens = model.generate(**input_tokens, streamer=streamer, max_new_tokens=100, do_sample=True, top_p=0.8)
+
