b20365b073906aff806ada9b3bce2ada468a0a6e
Pink <honganhnguyen915@gmail.com>
2024-08-28 14:58:06 +1000

    Modify Fine-Tuning_LoRA code

--- Finetuning/Fine-Tuning_LoRA.py
+++ Finetuning/Fine-Tuning_LoRA.py
@@ -8,10 +8,13 @@ from peft import PeftModel, PeftConfig, get_peft_model, LoraConfig
 
 import torch
 import numpy as np
+import evaluate
 
 def main():
     warnings.filterwarnings('ignore') # Ignore warnings when display the output
     
+    
+    # load base model
     model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
     
     model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", torch_dtype=torch.bfloat16)
@@ -28,10 +31,6 @@ def main():
     model.gradient_checkpointing_enable() # reduce number of stored activations
     model.enable_input_require_grads()
     
-    class CastOutputToFloat(nn.Sequential):
-        def forward(self, x): return super().forward(x).to(torch.float32)
-    model.lm_head = CastOutputToFloat(model.lm_head)    
-    
     #Setting up the LoRA adapter
     def print_trainable_parameters(model):
         trainable_params = 0
@@ -41,8 +40,9 @@ def main():
             all_params += param.numel()
             if param.requires_grad:
                 trainable_params += param.numel()
-        print(f"Trainable parameters: {trainable_params} || all params: {all_params} || trainable %: {100 * trainable_params}" )
+        print(f"Trainable parameters: {trainable_params} || all params: {all_params} || trainable %: {100 * trainable_params/all_params}" )
     
+    # LoRA config
     config = LoraConfig(
         r = 16,
         lora_alpha=32,
@@ -55,7 +55,33 @@ def main():
     print_trainable_parameters(model)
     
     # Load the dataset
-    data = load_dataset("lavita/medical-qa-shared-task-v1-toy")
+    data_files = {
+        "train": "Finetuning/Dataset/medical_1/english-train.json",
+        "test": "Finetuning/Dataset/medical_1/english-test.json",
+        "validation": "Finetuning/Dataset/medical_1/english-dev.json"
+    }
+    raw_dataset = load_dataset("json", data_files=data_files)
+    
+    # Tokenize the dataset
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], padding="max_length", truncation=True)
+    
+    tokenized_datasets = raw_dataset.map(tokenize_function, batched=True)
+    
+    # Training hyperparameters
+    training_args = TrainingArguments(output_dir="test_trainer")
+    
+    # evaluation
+    metric = evaluate.load("accuracy")
+    
+    def compute_metric(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+    
+    #training
+    trainer = Trainer(model = model, args = training_args, train_dataset= tokenized_datasets["train"], eval_dataset=tokenized_datasets["validation"], compute_metrics=compute_metric)
+    trainer.train()
 
 
 
