265ae8f810bc5d2592dfee8fe832cd634bb775dc
Pink <honganhnguyen915@gmail.com>
2024-08-30 10:04:33 +1000

    Add trainer_test (bert model) folder and wandb folder for evaluation and modify Fine-Tuning_LoRA code

--- Finetuning/Fine-Tuning_LoRA.py
+++ Finetuning/Fine-Tuning_LoRA.py
@@ -13,14 +13,19 @@ import evaluate
 def main():
     warnings.filterwarnings('ignore') # Ignore warnings when display the output
     
+    def get_device_map() -> str:
+        return 'cuda' if torch.cuda.is_available() else 'cpu'
     
+    device = get_device_map()
+
     # load base model
     model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
     
-    model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", torch_dtype=torch.bfloat16)
+    model = AutoModelForCausalLM.from_pretrained(model_id, device_map=device, torch_dtype=torch.bfloat16)
     
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
     
     # Freezing the original weights
     for param in model.parameters():
@@ -55,49 +60,28 @@ def main():
     model = get_peft_model(model, config)
     print_trainable_parameters(model)
     
-    #generation config
-    generation_config = {
-        "max_new_tokens": 200,
-        "_from_model_config ": True,
-        "bos_token_id": 1,
-        "eos_token_id": 11,
-        "pad_token_id": 11,
-        "temperature": 0.7,
-        "top_p": 0.7,
-    }
-    
-    #prompt
-    prompt = f"""
-    <human> Who is at risk for Lymphocytic Choriomeningitis (LCM)? ?
-    <assistant>:
-    """.strip()
-    
-    encoding = tokenizer(prompt, return_tensors="pt")
-    with torch.interface_mode():
-        outputs = model.generate(input_ids=encoding.input_ids, attention_mask=encoding.attention_mask, generation_config=generation_config)
-    
     # Load the dataset
-    dataset = load_dataset("csv", data_files="FineTuning/Dataset/medical_2/medDataset_processed.csv")
+    dataset = load_dataset("mlabonne/guanaco-llama2-1k", split="train")
     
-    # Tokenize the dataset
     def tokenize_function(examples):
-        return f"""
-    <human> {examples["Question"]}
-    <assistant>: {examples["Answer"]}
-    """.strip()
+        return tokenizer(examples["text"])
     
-    def generate_and_tokenize_prompt(examples):
-        full_prompt = tokenize_function(examples)
-        tokenized_full_prompt = tokenizer(full_prompt, padding=True, truncation=True)
-        return tokenized_full_prompt
-    
-    tokenized_datasets = dataset.map(generate_and_tokenize_prompt)
+    tokenized_dataset = dataset.map(
+        tokenize_function,
+        batched=True,
+        num_proc=4,
+        remove_columns=["text"]
+    )
     
     # Training hyperparameters
     training_args = TrainingArguments(output_dir="test_trainer")
     
     #training
-    trainer = Trainer(model = model, args = training_args, train_dataset= tokenized_datasets, data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False))
+    trainer = Trainer(
+        model=model,
+        train_dataset=tokenized_dataset,
+        args=training_args,
+    )
     trainer.train()
 
 
