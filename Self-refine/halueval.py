2c65b01f2a82b0101c851811b41dc1fee584b3db
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2024-09-22 22:37:14 +1000

    Add some print statement

--- Self-refine/halueval.py
+++ Self-refine/halueval.py
@@ -2,26 +2,31 @@
 Evaluating hallucination detection methods on HaluEval dataset, QA_samples subset.
 """
 
-
 import transformers, torch, datasets, evaluate, tqdm
-from Hallucination_detection import self_evaluation #, low_confidence_generation
+from Hallucination_detection import self_evaluation, low_confidence_generation
+
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 
 def load_model() -> transformers.PreTrainedModel:
-    return transformers.AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", device_map="auto", torch_dtype=torch.bfloat16)
+    return transformers.AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", torch_dtype=torch.bfloat16)
 
 def load_tokenizer() -> transformers.PreTrainedTokenizer:
-    return transformers.AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct") # type: ignore
+    return transformers.AutoTokenizer.from_pretrained(model_id) # type: ignore
 
 def load_terminators(tokenizer: transformers.PreTrainedTokenizer) -> list[int]:
     return [
         tokenizer.eos_token_id, # type: ignore
-        tokenizer.convert_tokens_to_ids("<|eot_id|>")
+        # tokenizer.convert_tokens_to_ids("<|eot_id|>")
     ]
 
+print("Loading model...")
 model = load_model()
+
+print("Loading tokenizer and terminators...")
 tokenizer = load_tokenizer()
 terminators = load_terminators(tokenizer)
 
+print("Loading dataset...")
 dataset = datasets.load_dataset("pminervini/HaluEval", "qa_samples", split="data")
 # Dataset({
 #     features: ['knowledge', 'question', 'answer', 'hallucination'],
@@ -35,8 +40,9 @@ results = {
     'targets': list(map(lambda x: 1 if x == "yes" else 0, dataset['hallucination'])),
 }
 
-# for method in ['self_evaluation', 'low_confidence_generation']:
-for method in ['self_evaluation']:
+for method in ['self_evaluation', 'low_confidence_generation']:
+
+    print(f"Running {method}...")
     results[method] = []
 
     for knowledge, question, answer in tqdm.tqdm(zip(dataset['knowledge'], dataset['question'], dataset['answer']), method):
@@ -46,9 +52,9 @@ for method in ['self_evaluation']:
         question_with_context = f"{knowledge} {question}"
 
         if method == 'self_evaluation':
-            predict = self_evaluation(question_with_context, answer, 10, model, tokenizer, terminators)
-        # elif method == 'low_confidence_generation':
-        #     predict = low_confidence_generation(question_with_context, answer, model, tokenizer, terminators)
+            predict = self_evaluation(question_with_context, answer, 5, model, tokenizer, terminators)
+        elif method == 'low_confidence_generation':
+            predict = low_confidence_generation(question_with_context, answer, model, tokenizer, terminators)
 
         results[method].append(int(predict))
 
@@ -56,8 +62,9 @@ metrics = evaluate.combine(['accuracy', 'f1', 'precision', 'recall'])
 
 scores = {}
 
-# for method in ['self_evaluation', 'low_confidence_generation']:
-for method in ['self_evaluation']:
+for method in ['self_evaluation', 'low_confidence_generation']:
+
+    print(f"Computing scores for {method}...")
     scores[method] = metrics.compute(results['targets'], results[method])
 
 print(scores)
