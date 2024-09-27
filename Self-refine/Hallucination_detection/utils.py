fd1fe160c538721ecd05bbaa1a8c8ba7cde1833a
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2024-09-28 05:17:39 +1000

    Calculate log prob of given string using model.forward() instead of model.generate()
    I'm dumb

--- Self-refine/Hallucination_detection/utils.py
+++ Self-refine/Hallucination_detection/utils.py
@@ -1,90 +1,33 @@
-import transformers, torch
+import torch
 
-def compute_transition_scores_from_string(model, tokenizer, terminators, string_tokens, start_idx=0):
+def compute_log_prob_from_string(model, string_tokens, start_idx=0):
     """
-    Manually compute the transition scores for a string by generating one token at a time.
+    Manually compute the log probability of a string.
 
     Args:
         model: The model to use for generation.
-        tokenizer: The tokenizer to use for generation.
-        terminators: A list of token IDs that indicate the end of a sequence.
         string_tokens: The input string tokens of the full generation.
         start_idx: The index of the start of the answer.
 
     Returns:
-        The transition scores for the string.
+        The log probability of the string.
     """
+    # sequence_length: length of full string tokens = string_tokens.shape[-1]
+    # answer_length: length of the answer = sequence_length - start_idx
 
-    # logits = ()
-    # for i in range(start_idx, string_tokens.shape[-1] - 1):
-    #     logit = model.generate(
-    #         string_tokens[:, :i],
-    #         max_new_tokens=1,
-    #         output_scores=True,
-    #         return_dict_in_generate=True,
-    #         eos_token_id=terminators,
-    #     )
-    #     logits += logit.scores
+    # string_tokens: tensor(batch_size, sequence_length)
 
-    # do the above in batch
+    # logits: tensor(batch_size, answer_length, vocab_size)
+    logits: torch.Tensor = model(string_tokens, return_dict=True, num_logits_to_keep=string_tokens.shape[-1] - start_idx).logits
+    logits = logits.squeeze(dim=0) # remove batch dimension, tensor(answer_length, vocab_size)
+    logits = logits.log_softmax(dim=-1) # convert logits to log probabilities
 
-    # Example:
-    # Given tokens: tensor([[token1, token2, token3, token4, token5]])
+    string_tokens = string_tokens.squeeze(0) # remove batch dimension, tensor(sequence_length)
+    string_tokens = string_tokens[start_idx:] # remove the prefix tokens, tensor(answer_length)
 
-    generation_batch = torch.cat([
-        torch.cat([
-            # left pad with eos tokens
-            torch.ones((string_tokens.shape[0], string_tokens.shape[1] - i - 1), dtype = string_tokens.dtype, device=model.device) * tokenizer.eos_token_id,
-            string_tokens[:, :i]
-        ], dim=-1)
-        for i in range(start_idx, string_tokens.shape[-1])
-    ], dim=0)
-    # Example:
-    # tensor([[<|eos|>, <|eos|>, token1, token2],
-    #         [<|eos|>,  token1, token2, token3],
-    #         [ token1,  token2, token3, token4]])
+    log_prob = logits[torch.arange(logits.shape[0]), string_tokens] # tensor(answer_length)
 
-    generation_attention_mask = torch.cat([
-        torch.cat([
-            torch.zeros((string_tokens.shape[0], string_tokens.shape[1] - i - 1), dtype = string_tokens.dtype, device=model.device),
-            torch.ones((string_tokens.shape[0], i), dtype = string_tokens.dtype, device=model.device)
-        ], dim=-1)
-        for i in range(start_idx, string_tokens.shape[-1])
-    ], dim=0)
-    # Example:
-    # tensor([[0, 0, 1, 1],
-    #         [0, 1, 1, 1],
-    #         [1, 1, 1, 1]])
-
-    generation_logits = model.generate(
-        input_ids=generation_batch,
-        attention_mask=generation_attention_mask,
-        max_new_tokens=1,
-        output_scores=True,
-        return_dict_in_generate=True,
-        eos_token_id=terminators,
-    )
-    # Example:
-    # generation_logits.sequences                vvvvvv -> this is the token we generated
-    # tensor([[<|eos|>, <|eos|>, token1, token2, token3],
-    #         [<|eos|>,  token1, token2, token3, token4],
-    #         [ token1,  token2, token3, token4, token5]])
-    # generation_logits.scores
-    # (tensor([[-84.0000, -84.0000, -88.5000,  ..., -93.0000, -93.5000, -87.0000],
-    #          [-74.0000, -73.0000, -77.5000,  ..., -77.5000, -81.5000, -75.5000],
-    #          [-84.0000, -82.0000, -85.5000,  ..., -84.0000, -88.5000, -81.5000]]),
-    # )
-
-    generation_scores = torch.split(generation_logits.scores[0], 1)
-    # The score should be tuple of tensors, each tensor corresponding to the score of the generated token
-    # (tensor([[-84.0000, -84.0000, -88.5000,  ..., -93.0000, -93.5000, -87.0000]]),
-    #  tensor([[-74.0000, -73.0000, -77.5000,  ..., -77.5000, -81.5000, -75.5000]]),
-    #  tensor([[-83.5000, -81.5000, -85.0000,  ..., -83.5000, -88.0000, -81.5000]])
-    # )
-
-    transition_scores = model.compute_transition_scores(string_tokens, generation_scores, normalize_logits=True)
-
-    return transition_scores.squeeze(0)
+    return log_prob
 
 
 
