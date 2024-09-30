273416efb325a281efc16a2369887e10480f4e98
Pink <honganhnguyen915@gmail.com>
2024-10-01 07:33:48 +1000

    Restructure folder and rewrite and adjust Processing code

---
+++ Finetuning/Other code/cleaning.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def main():
+    data = pd.read_csv('../medical_3/Deep Cleaning/Version_1.csv')
+    
+    rows = []
+    for i in data.iloc:
+        rows.append(
+            {
+                "question": i["question"],
+                "option_a": i['opa'],
+                "option_b": i['opb'],
+                "option_c": i['opc'],
+                "option_d": i['opd'],
+                "cop": i['cop'],
+                "exp": i["exp"],
+                "subject": i["subject_name"],
+            }
+        )
+        
+    df = pd.DataFrame(rows)
+    
+    
+    # Replace specified characters with space in all columns
+    for column in df.columns:
+        df[column] = df[column].str.replace(r'<\\p>|<p>|&;|<img alt="" src="/>', ' ', regex=True)
+    
+    # Count rows containing specified characters after replacement
+    count = df.apply(lambda x: x.str.contains(r'<\\p>|<p>|&;|<img alt="" src="/>').any(), axis=1).sum()
+    print(f"Rows containing specified characters after replacement: {count}")
+    
+    # Verify replacement
+    print(df)
+    
+if __name__ == '__main__':
+    main()
+
