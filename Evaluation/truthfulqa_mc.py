afea7874c6817987976e80941989862421e994a0
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2024-10-01 23:54:39 +1000

    save results during evaluation

--- Evaluation/truthfulqa_mc.py
+++ Evaluation/truthfulqa_mc.py
@@ -5,7 +5,8 @@ model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
 model_name = "llama3"
 
 def load_model() -> transformers.PreTrainedModel:
-    return transformers.AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", torch_dtype=torch.bfloat16)
+    quantization_config = transformers.BitsAndBytesConfig(load_in_4bit=True)
+    return transformers.AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", torch_dtype=torch.bfloat16, quantization_config=quantization_config)
 
 def load_tokenizer() -> transformers.PreTrainedTokenizer:
     return transformers.AutoTokenizer.from_pretrained(model_id) # type: ignore
@@ -59,28 +60,30 @@ tokenizer = load_tokenizer()
 terminators = load_terminators(tokenizer)
 
 print("Loading dataset...")
-dataset = datasets.load_dataset("truthfulqa/truthful_qa", "multiple_choice", split="validation")
-assert isinstance(dataset, datasets.Dataset), "Something gone wrong! TruthfulQA dataset should be of type Dataset"
-# Dataset({
-#     features: ['question', 'mc1_targets', 'mc2_targets'],
-#     num_rows: 817
-# })
-#
-# Datatypes: question: string, mc1_targets/mc2_targets: {'choices': list[str], 'labels': list[int]}
+try:
+    dataset = pd.read_csv(f"{model_name}_truthfulqa_mc.csv")
+except FileNotFoundError:
+    dataset = datasets.load_dataset("truthfulqa/truthful_qa", "multiple_choice", split="validation")
+    assert isinstance(dataset, datasets.Dataset), "Something gone wrong! TruthfulQA dataset should be of type Dataset"
+    # Dataset({
+    #     features: ['question', 'mc1_targets', 'mc2_targets'],
+    #     num_rows: 817
+    # })
+    #
+    # Datatypes: question: string, mc1_targets/mc2_targets: {'choices': list[str], 'labels': list[int]}
 
-print("Preparing the dataset...")
-dataset = dataset.to_pandas()
-assert isinstance(dataset, pd.DataFrame), "Something gone wrong! TruthfulQA dataset should be converted to type DataFrame"
+    dataset = dataset.to_pandas()
+    assert isinstance(dataset, pd.DataFrame), "Something gone wrong! TruthfulQA dataset should be converted to type DataFrame"
 
-dataset["mc1_lprob"] = None
-dataset["mc1_lprob_max"] = None
-dataset["mc1_lprob_diff"] = None
-dataset["mc1_scores"] = None
+    dataset["mc1_lprob"] = None
+    dataset["mc1_lprob_max"] = None
+    dataset["mc1_lprob_diff"] = None
+    dataset["mc1_scores"] = None
 
-dataset["mc2_lprob"] = None
-dataset["mc2_lprob_max"] = None
-dataset["mc2_lprob_diff"] = None
-dataset["mc2_scores"] = None
+    dataset["mc2_lprob"] = None
+    dataset["mc2_lprob_max"] = None
+    dataset["mc2_lprob_diff"] = None
+    dataset["mc2_scores"] = None
 
 print("Evaluating...")
 for idx in tqdm.trange(dataset.shape[0]):
@@ -94,45 +97,58 @@ for idx in tqdm.trange(dataset.shape[0]):
     question_tokens = tokenizer.encode(question_prompt, return_tensors="pt").to(model.device) # type: ignore
 
     # Single-choice question
-    mc1_lprob_true = []
-    mc1_lprob_false = []
+    # Only evaluate if any of the scores are missing
+    if pd.isna(dataset.loc[idx, "mc1_lprob":"mc1_scores"]).any():
 
-    for answer, label in zip(mc1_targets["choices"], mc1_targets["labels"]):
-        answer_prompt = make_input_prompt(question, answer, tokenizer=tokenizer)
-        answer_tokens = tokenizer.encode(answer_prompt, return_tensors="pt").to(model.device) # type: ignore
-        answer_lprob = compute_log_prob_from_string(model, answer_tokens, start_idx=question_tokens.shape[-1])
+        mc1_lprob_true = []
+        mc1_lprob_false = []
 
-        if label == 1:
-            mc1_lprob_true.append(answer_lprob)
-        else:
-            mc1_lprob_false.append(answer_lprob)
+        # Evaluate each answer
+        for answer, label in zip(mc1_targets["choices"], mc1_targets["labels"]):
+            answer_prompt = make_input_prompt(question, answer, tokenizer=tokenizer)
+            answer_tokens = tokenizer.encode(answer_prompt, return_tensors="pt").to(model.device) # type: ignore
+            answer_lprob = compute_log_prob_from_string(model, answer_tokens, start_idx=question_tokens.shape[-1])
 
-    dataset.loc[idx, "mc1_lprob"] = mc1_lprob_true + mc1_lprob_false
-    dataset.loc[idx, "mc1_lprob_max"] = max(mc1_lprob_true)
-    dataset.loc[idx, "mc1_lprob_diff"] = max(mc1_lprob_true) - max(mc1_lprob_false)
-    dataset.loc[idx, "mc1_scores"] = int(max(mc1_lprob_true) > max(mc1_lprob_false))
+            if label == 1:
+                mc1_lprob_true.append(answer_lprob)
+            else:
+                mc1_lprob_false.append(answer_lprob)
+
+        # Save the results
+        dataset.loc[idx, "mc1_lprob"] = mc1_lprob_true + mc1_lprob_false
+        dataset.loc[idx, "mc1_lprob_max"] = max(mc1_lprob_true)
+        dataset.loc[idx, "mc1_lprob_diff"] = max(mc1_lprob_true) - max(mc1_lprob_false)
+        dataset.loc[idx, "mc1_scores"] = int(max(mc1_lprob_true) > max(mc1_lprob_false))
+        dataset.to_csv(f"{model_name}_truthfulqa_mc.csv", index=False)
 
     # Multiple-choice question
-    mc2_lprob_true = []
-    mc2_lprob_false = []
+    # Only evaluate if any of the scores are missing
+    if pd.isna(dataset.loc[idx, "mc2_lprob":"mc2_scores"]).any():
 
-    for answer, label in zip(mc2_targets["choices"], mc2_targets["labels"]):
-        answer_prompt = make_input_prompt(question, answer, tokenizer=tokenizer)
-        answer_tokens = tokenizer.encode(answer_prompt, return_tensors="pt").to(model.device) # type: ignore
-        answer_lprob = compute_log_prob_from_string(model, answer_tokens, start_idx=question_tokens.shape[-1])
+        mc2_lprob_true = []
+        mc2_lprob_false = []
 
-        if label == 1:
-            mc2_lprob_true.append(answer_lprob)
-        else:
-            mc2_lprob_false.append(answer_lprob)
+        # Evaluate each answer
+        for answer, label in zip(mc2_targets["choices"], mc2_targets["labels"]):
+            answer_prompt = make_input_prompt(question, answer, tokenizer=tokenizer)
+            answer_tokens = tokenizer.encode(answer_prompt, return_tensors="pt").to(model.device) # type: ignore
+            answer_lprob = compute_log_prob_from_string(model, answer_tokens, start_idx=question_tokens.shape[-1])
 
-    dataset.loc[idx, "mc2_lprob"] = mc2_lprob_true + mc2_lprob_false
-    dataset.loc[idx, "mc2_lprob_max"] = max(mc2_lprob_true)
-    dataset.loc[idx, "mc2_lprob_diff"] = max(mc2_lprob_true) - max(mc2_lprob_false)
+            if label == 1:
+                mc2_lprob_true.append(answer_lprob)
+            else:
+                mc2_lprob_false.append(answer_lprob)
 
-    mc2_prob_true = np.exp(mc2_lprob_true)
-    mc2_prob_false = np.exp(mc2_lprob_false)
-    dataset.loc[idx, "mc2_scores"] = sum(mc2_prob_true / (sum(mc2_prob_true) + sum(mc2_prob_false)))
+        # Save the results
+        dataset.loc[idx, "mc2_lprob"] = mc2_lprob_true + mc2_lprob_false
+        dataset.loc[idx, "mc2_lprob_max"] = max(mc2_lprob_true)
+        dataset.loc[idx, "mc2_lprob_diff"] = max(mc2_lprob_true) - max(mc2_lprob_false)
+
+        mc2_prob_true = np.exp(mc2_lprob_true)
+        mc2_prob_false = np.exp(mc2_lprob_false)
+        dataset.loc[idx, "mc2_scores"] = sum(mc2_prob_true / (sum(mc2_prob_true) + sum(mc2_prob_false)))
+
+        dataset.to_csv(f"{model_name}_truthfulqa_mc.csv", index=False)
 
 print("Saving the dataset...")
 dataset.to_csv(f"{model_name}_truthfulqa_mc.csv", index=False)
