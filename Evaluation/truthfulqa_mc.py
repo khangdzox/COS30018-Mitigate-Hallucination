1936b65bcc5b5c216870ffe0fb6ce84e8b3beaa6
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2024-10-05 03:53:32 +1000

    uncomment terminators

--- Evaluation/truthfulqa_mc.py
+++ Evaluation/truthfulqa_mc.py
@@ -14,7 +14,7 @@ def load_tokenizer() -> transformers.PreTrainedTokenizer:
 def load_terminators(tokenizer: transformers.PreTrainedTokenizer) -> list[int]:
     return [
         tokenizer.eos_token_id, # type: ignore
-        # tokenizer.convert_tokens_to_ids("<|eot_id|>")
+        tokenizer.convert_tokens_to_ids("<|eot_id|>")
     ]
 
 def make_input_prompt(question: str, answer = '') -> str:
