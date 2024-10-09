29b93269200468f276631be32d8c2d9ec6f34589
Saw <sawkokooo.sakuraboutique@gmail.com>
2024-10-10 03:30:00 +1100

    selfcheckGPT

--- hallucination_detection/detection/selfcheckGPT.py
+++ hallucination_detection/detection/selfcheckGPT.py
@@ -1,47 +1,22 @@
 import numpy as np
-import spacy
-from transformers import pipeline
 import torch
 from selfcheckgpt.modeling_selfcheck import SelfCheckLLMPrompt
+from transformers import pipeline
+import spacy
 
-def initialize_pipeline(device_type="auto"):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B-Instruct", device_map=device_type)
-    return pipe, device
+def check_hallucination(question, answer, model_id, num_samples=1, device=torch.device("cuda")):
+    self_check = SelfCheckLLMPrompt(model=model_id, device=device)
+    pipe = pipeline("text-generation", model=model_id, device_map="auto")
+    samples = pipe([question] * num_samples, temperature=1.0, do_sample=True, max_new_tokens=50, return_full_text=False)
+    generated_samples = [sample[0]["generated_text"] for sample in samples]
 
-def generate_response(pipe, prompt, sample=False, num_samples=1):
-    if sample:
-        responses = pipe([prompt] * num_samples, temperature=1.0, do_sample=True, max_new_tokens=20, return_full_text=False)
-        return [response[0]["generated_text"] for response in responses]
-    else:
-        response = pipe(prompt, do_sample=False, max_new_tokens=10, return_full_text=False)
-        return response[0]["generated_text"]
-
-def detect_hallucination(sentences, samples, model_name="meta-llama/Meta-Llama-3-8B-Instruct", device=torch.device("cpu")):
-    self_check = SelfCheckLLMPrompt(model_name, device=device)
-    sent_scores = self_check.predict(sentences=sentences, sampled_passages=samples, verbose=False)
-    hallucination_score = np.mean(sent_scores)
-    if hallucination_score < 0.5:
-        return "0"
-    else:
-        return "1"
-     
-
-def process_text(nlp, text):
-    return [sent.text.strip() for sent in nlp(text).sents]
-
-def main():
-    pipe, device = initialize_pipeline()
     nlp = spacy.load("en_core_web_sm")
-    
-    prompt = "23 divided by 2 is 11.\nAnswer:"
-    response = generate_response(pipe, prompt)
-    samples = generate_response(pipe, prompt, sample=True, num_samples=1)
-    
-    sentences = process_text(nlp, response)
-    
-    is_hallucination = detect_hallucination(sentences, samples, device=device)
-    return is_hallucination
+    Response = " ".join(generated_samples)
+    sentences = [
+            sent.text.strip() for sent in nlp(Response).sents
+        ]  # spacy sentence tokenization
+    sent_scores = self_check.predict(sentences=sentences, sampled_passages=generated_samples, verbose=False)
+    hallucination_score = np.mean(sent_scores)
 
-if __name__ == "__main__":
-    print(main())
+    return hallucination_score >= 0.5
+
