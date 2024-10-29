e0a8503c797fded6fac41e9ca3a39781ca0a2681
Khang Vo <88926702+khangdzox@users.noreply.github.com>
2024-10-30 05:06:18 +1100

    update gradio app

--- app.py
+++ app.py
@@ -28,23 +28,33 @@ terminators = [
     tokenizer.convert_tokens_to_ids("<|eot_id|>")
 ]
 
-def change_method(method, prev):
-    global model
-
-    if method != prev:
-        del model
+def change_model(current_model: gr.State, current_selection, new_selection):
+    if current_selection != new_selection:
+        current_model.value = None
         gc.collect()
         torch.cuda.empty_cache()
 
-        if method == 'Self-refine':
-            model = load_model(model_id)
-        elif method == 'Fine-tune':
-            model = load_model(model_id, lora_ver)
+        if new_selection == 'Base model':
+            return load_model(model_id), new_selection
 
-    return method
+        return load_model(model_id, lora_ver), new_selection
 
-def generate(message, history):
-    return self_refine(message, model, tokenizer, terminators)
+def changee_model_selection(current_selection, new_selection):
+    return gr.Button(interactive = new_selection != current_selection)
+
+def generate(message, chat_history, model, method):
+    if method == "No method":
+        template = [{"role": "user", "content": message}]
+        input_tokens = tokenizer.apply_chat_template(template, return_tensors="pt").to(model.device) # type: ignore
+        output_tokens = model.generate(input_tokens, max_new_tokens=200, eos_token_id=terminators, top_k=1) #type: ignore
+        response = tokenizer.decode(output_tokens[0, input_tokens.shape[-1]:], skip_special_tokens=True)
+    else:
+        response = self_refine(message, model, tokenizer, terminators, max_new_tokens=200)
+
+    chat_history.append({"role": "user", "content": message})
+    chat_history.append({"role": "assistant", "content": response})
+
+    return "", chat_history
 
 chat = gr.ChatInterface(
     generate,
@@ -53,12 +63,26 @@ chat = gr.ChatInterface(
 )
 
 with gr.Blocks(fill_height=True) as app:
+    model_state = gr.State(lambda: model)
+    current_model_selection = gr.State("Base model")
 
-    method_selection = gr.Dropdown(["Self-refine", "Fine-tune"], label="Method")
-    method_selection.change(
-        change_method, [method_selection, gr.State([])], [gr.State()]
+    with gr.Row(equal_height=True):
+        method = gr.Dropdown(["Self-refine", "No method"], label="Method", container=False)
+        model_selection = gr.Dropdown(["Base model", "Fine-tuned model"], label="Model", container=False)
+        change_model_btn = gr.Button("Change model")
+
+    model_selection.change(
+        changee_model_selection, [current_model_selection, model_selection], [change_model_btn]
     )
+    change_model_btn.click(
+        change_model, [model_state, current_model_selection, model_selection], [model_state, current_model_selection]
+    )
+    change_model_btn.interactive = False
 
-    chat.render()
+    chatbot = gr.Chatbot(type="messages", height=200)
+    msg = gr.Textbox(submit_btn=True, label="Message", placeholder="Type your message here")
+    clear = gr.ClearButton(value="Clear Chat")
+
+    msg.submit(generate, [msg, chatbot, model_state, method], [msg, chatbot])
 
 app.launch(share=True)
